db7d29e5ac17300f8445e0565056fa4647da6510
Gursaanj <gursaanjresides@gmail.com>
2020-12-16 23:36:05 -0800

    Got rid of validtor menuitems

--- ObjectReplacerAndRenamer/Assets/ObjectReplacerAndRenamer/Scripts/Editor/EditorMenus.cs
+++ ObjectReplacerAndRenamer/Assets/ObjectReplacerAndRenamer/Scripts/Editor/EditorMenus.cs
@@ -6,7 +6,7 @@ namespace GursaanjTools
     public static class EditorMenus
     {
         #region Object Replacer Tool
-
+        
         [MenuItem("GursaanjTools/GameObject Tools/Replace Selected Objects")]
         public static void ReplaceObjectsTool()
         {
@@ -17,13 +17,6 @@ namespace GursaanjTools
 
         #region Object Renamer
 
-        [MenuItem("GursaanjTools/GameObject Tools/Rename Selected Objects", true)]
-        public static bool RenameObjectsToolValidator()
-        {
-            GameObject[] selection = Selection.gameObjects;
-            return selection != null && selection.Length != 0;
-        }
-    
         [MenuItem("GursaanjTools/GameObject Tools/Rename Selected Objects")]
         public static void RenameObjectsTool()
         {

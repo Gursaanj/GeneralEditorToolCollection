c0a23bde0b24f0930bd0384c589c39d07cdd7b99
Gursaanj <gursaanjresides@gmail.com>
2020-12-19 01:07:28 -0800

    Semi-Working Parent Class
    Working parent class, tasked with handling general properties and method shared with all gui control related editor windows
    
    Still  need to handle Init Window in a smart way, create class for ControlGUI to live

--- ObjectReplacerAndRenamer/Assets/ObjectReplacerAndRenamer/Scripts/Editor/GUIControlEditorWindow.cs
+++ ObjectReplacerAndRenamer/Assets/ObjectReplacerAndRenamer/Scripts/Editor/GUIControlEditorWindow.cs
@@ -1,33 +1,111 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 namespace GursaanjTools
 {
+    //TODO : Handle creation in another window - Not static
     public abstract class GuiControlEditorWindow : EditorWindow
     {
-        //Have ReturnPressed
-        //Have FocusControl and ControlSwitching
-        //Handle creation in another window - Not static
-
         #region Variables
 
-        //protected List<string> _listOfControls = new List<string>();
+        protected static GuiControlEditorWindow _window;
+        
+        //GUI Labels
+        protected const string SelectionCountString = "Selection Count: ";
+        protected const string CastedCountFormat = "000";
+        
+        //Warning Messages
+        protected const string ErrorTitle = "Error";
+        protected const string ConfirmationMessage = "Sounds good";
+        protected const string CancellationMessage = "Actually, no!";
+
+
+        protected GameObject[] _selectedGameObjects;
+
+        protected List<string> _listOfControls = new List<string>();
+        
+        private bool _shouldFocusOnTextField = true;
+        private int _currentControlIndex = 0;
+
+        #endregion
+
+        #region Properties
 
         #endregion
         
         #region Builtin Methods
-        
+
+        protected void OnGUI()
+        {
+            _selectedGameObjects = Selection.gameObjects;
+            CreateGUI();
+            FinishGUICycle();
+        }
+
         #endregion
 
         #region Custom Methods
+
+        protected abstract void CreateGUI();
+        
+        protected void FinishGUICycle()
+        {
+            FocusOnTextField();
+
+            if (_window != null)
+            {
+                _window.Repaint();
+            }
+        }
 
         protected bool IsReturnPressed()
         {
             Event current = Event.current;
             return current.isKey && current.keyCode == KeyCode.Return;
         }
-        
+
+        protected void FocusOnTextField()
+        {
+            if (_listOfControls != null && _listOfControls.Count > 0)
+            {
+                ChangeCurrentControl();
+
+                if (_shouldFocusOnTextField && _window != null)
+                {
+                    _window.Focus();
+                    EditorGUI.FocusTextInControl(_listOfControls[_currentControlIndex]);
+                    _shouldFocusOnTextField = false;
+                }
+            }
+        }
+
+        private void ChangeCurrentControl()
+        {
+            Event currentEvent = Event.current;
+
+            if (currentEvent.isKey)
+            {
+                if (currentEvent.keyCode == KeyCode.DownArrow)
+                {
+                    if (_currentControlIndex < (_listOfControls.Count - 1))
+                    {
+                        _currentControlIndex++;
+                        _shouldFocusOnTextField = true;
+                    }
+                }
+                else if (currentEvent.keyCode == KeyCode.UpArrow)
+                {
+                    if (_currentControlIndex > 0)
+                    {
+                        _currentControlIndex--;
+                        _shouldFocusOnTextField = true;
+                    }
+                }
+            }
+        }
+
         #endregion
     }
 }
--- ObjectReplacerAndRenamer/Assets/ObjectReplacerAndRenamer/Scripts/Editor/GroupObjects_Editor.cs
+++ ObjectReplacerAndRenamer/Assets/ObjectReplacerAndRenamer/Scripts/Editor/GroupObjects_Editor.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using UnityEditor;
-using UnityEditor.Graphs;
+﻿using UnityEditor;
 using UnityEngine;
 
 namespace GursaanjTools
@@ -8,33 +6,28 @@ namespace GursaanjTools
     public class GroupObjects_Editor : GuiControlEditorWindow
     {
         #region Variables
-    
-        private static GroupObjects_Editor _window = null;
+        
         private static readonly Vector2 MinSize = new Vector2(300,140);
         private static readonly Vector2 MaxSize = new Vector2(300,140);
         
+        //GUI Labels
         private const string GroupSelectedObjects = "Group Selected Objects";
-        private const string SelectionCountString = "Selection Count: ";
-        private const string CastedCountFormat = "000";
         private const string GroupObjectsLabel = "Enter Group Name";
         private const string CreateGroupLabel = "Group Objects";
+        
+        //Warning Messages
+        private const string NothingSelectedWarning = "No objects to Group!";
+        private const string NoGroupNameWarning = "No Group name entered! Would you like to continue?";
+        private const string DifferentParentsWarning = "Two or more objects have different parents! Unable to Group!";
+        
+        //ControlNames
+        private const string GroupNameControl = "groupNameControl";
+        
+        //Undo Labels
         private const string UndoGroupingLabel = "Grouping";
         
-        private const string ErrorTitle = "Error";
-        private const string NothingSelectedWarning = "No objects to Group!";
-        private const string NogroupNameWarning = "No Group name entered! Would you like to continue?";
-        private const string DifferentParentsWarning = "Two or more objects have different parents! Unable to Group!";
-        private const string ConfirmationMessage = "Sounds good";
-        private const string CancellationMessage = "Actually, no!";
-
-        private const string GroupNameControl = "groupNameControl";
-
-        private GameObject[] _selectedGameObjects;
+        
         private string _groupName = "Group";
-
-        private bool _shouldFocusOnTextField = true;
-        private int _currentControlIndex = 0;
-        private List<string> _listOfControls = new List<string>();
         #endregion
     
         #region Unity Methods
@@ -49,12 +42,11 @@ namespace GursaanjTools
             _window.Focus();
             _window.Show();
         }
-    
-        private void OnGUI()
-        {
-            _selectedGameObjects = Selection.gameObjects;
-            EditorGUILayout.LabelField($"{SelectionCountString}{_selectedGameObjects.Length.ToString(CastedCountFormat)}");
 
+        protected override void CreateGUI()
+        {
+            EditorGUILayout.LabelField($"{SelectionCountString}{_selectedGameObjects.Length.ToString(CastedCountFormat)}");
+            
             using (new EditorGUILayout.HorizontalScope())
             {
                 EditorGUILayout.Space();
@@ -66,31 +58,22 @@ namespace GursaanjTools
                     CreateControlledTextField(GroupNameControl, ref _groupName);
                     
                     EditorGUILayout.Space();
-
+            
                     if (GUILayout.Button(CreateGroupLabel, GUILayout.ExpandWidth(true), GUILayout.Height(40f)) || IsReturnPressed())
                     {
                         GroupObjects();
                     }
-
+            
                     EditorGUILayout.Space();
                 }
                 EditorGUILayout.Space();
             }
-            
-            ChangeCurrentControl();
-            FocusOnTextField();
-
-            if (_window != null)
-            {
-                _window.Repaint();
-            }
         }
-    
+
         #endregion
     
         #region Custom Methods
         
-        //TODO : Ensure new groups are parented appropriately 
         private void GroupObjects()
         {
             if (_selectedGameObjects == null || _selectedGameObjects.Length == 0)
@@ -101,7 +84,7 @@ namespace GursaanjTools
 
             if (string.IsNullOrEmpty(_groupName))
             {
-                if (!EditorUtility.DisplayDialog(ErrorTitle, NogroupNameWarning, ConfirmationMessage,
+                if (!EditorUtility.DisplayDialog(ErrorTitle, NoGroupNameWarning, ConfirmationMessage,
                     CancellationMessage))
                 {
                     return;
@@ -150,42 +133,7 @@ namespace GursaanjTools
                 _listOfControls.Add(controlName);
             }
         }
-        
-        private void ChangeCurrentControl()
-        {
-            Event currentEvent = Event.current;
 
-            if (currentEvent.isKey)
-            {
-                if (currentEvent.keyCode == KeyCode.DownArrow)
-                {
-                    if (_currentControlIndex < (_listOfControls.Count - 1))
-                    {
-                        _currentControlIndex++;
-                        _shouldFocusOnTextField = true;
-                    }
-                }
-                else if (currentEvent.keyCode == KeyCode.UpArrow)
-                {
-                    if (_currentControlIndex > 0)
-                    {
-                        _currentControlIndex--;
-                        _shouldFocusOnTextField = true;
-                    }
-                }
-            }
-        }
-
-        private void FocusOnTextField()
-        {
-            if(_shouldFocusOnTextField && _window != null && _listOfControls != null && _listOfControls.Count != 0)
-            {
-                _window.Focus();
-                EditorGUI.FocusTextInControl(_listOfControls[_currentControlIndex]);
-                _shouldFocusOnTextField = false;
-            }
-        }
-    
         #endregion
     }
 }

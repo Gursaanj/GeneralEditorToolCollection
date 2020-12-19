594e910da22f368241e96132b0dba82f468672c2
Gursaanj <gursaanjresides@gmail.com>
2020-12-18 21:15:02 -0800

    Initiated GUI_control parent class
    To take care of all GUI_control and common event registration  events

---
+++ ObjectReplacerAndRenamer/Assets/ObjectReplacerAndRenamer/Scripts/Editor/GUIControl_EditorWindow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace GursaanjTools
+{
+    public abstract class GUIControl_EditorWindow : EditorWindow
+    {
+        //Have ReturnPressed
+        //Have FocusControl and ControlSwitching
+        //Handle creation in another window - Not static
+
+        #region Variables
+
+        //protected List<string> _listOfControls = new List<string>();
+
+        #endregion
+        
+        #region Builtin Methods
+        
+        #endregion
+
+        #region Custom Methods
+
+        protected bool IsReturnPressed()
+        {
+            Event current = Event.current;
+            return current.isKey && current.keyCode == KeyCode.Return;
+        }
+        
+        #endregion
+    }
+}
+
